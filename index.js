311d6fe33e1e866955fb0c5f542e6922e4a8ee0a
bassarcy <bassarcy@gmail.com>
2023-10-05 15:56:24 -0500

    Added delete item function

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ addButton.addEventListener("click", () => {
     let newItem = document.createElement("li")
     let newItemText = document.createTextNode(taskInput)
     newItem.appendChild(newItemText)
+    newItem.classList.add("new-item")
     itemList = document.getElementById("list")
     itemList.appendChild(newItem)
+    newItem.addEventListener("click", () => {
+        let removePrompt = window.confirm("Would you like to delete this item?")
+        if(removePrompt) {
+            newItem.remove()
+        }
+    })
 })
+
+
+
+
     
